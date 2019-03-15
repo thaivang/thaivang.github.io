56409a9758725b60f207f5b21ab24b1519f5ab04
Thai Vang <thaivangxd@gmail.com>
2019-03-15 14:31:47 -0700

    style changes to border and nav-bar icons

---
+++ js/playlist.js
@@ -0,0 +1,13 @@
+var playList = ["BTS - Fire", "BTS - Fake Love", "Utada Hikaru - Let Me Face My Fears", "Utada Hikaru - Simple and clean", "LiSA - Crossing Fields", "Maroon 5 - Sugar"];
+
+function print (songs){
+    document.write(songs);
+}
+
+//prints out the array of items 
+function printList(playList) {
+    var listHTML = '<ol>';
+    for (var i = 0 ; i <playList.length; i++){
+        
+    }
+}
