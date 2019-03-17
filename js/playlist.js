b7c4884b4355bd616a8f637759883301df247c14
Thai Vang <thaivangxd@gmail.com>
2019-03-16 22:03:56 -0700

    Adjust social icons and add fixed navbar

--- js/playlist.js
+++ js/playlist.js
@@ -1,13 +1,15 @@
-var playList = ["BTS - Fire", "BTS - Fake Love", "Utada Hikaru - Let Me Face My Fears", "Utada Hikaru - Simple and clean", "LiSA - Crossing Fields", "Maroon 5 - Sugar"];
+// var playList = ["BTS - Fire", "BTS - Fake Love", "Utada Hikaru - Let Me Face My Fears", "Utada Hikaru - Simple and clean", "LiSA - Crossing Fields", "Maroon 5 - Sugar"];
 
-function print (songs){
-    document.write(songs);
-}
+// function print (songs){
+//     document.write(songs);
+// }
 
-//prints out the array of items 
-function printList(playList) {
-    var listHTML = '<ol>';
-    for (var i = 0 ; i <playList.length; i++){
+// //prints out the array of items 
+// function printList(playList) {
+//     var listHTML = '<ol>';
+//     for (var i = 0 ; i <playList.length; i++){
         
-    }
-}
+//     }
+// }
+
+//add functioning code for music playlist
